0ffb82d86edd872553751c6d3f638a665eca85a3
Ishika_Upadhyay <158724315+Ishika1614@users.noreply.github.com>
2026-02-06 20:00:38 +0530

    Add Next Greater Element using stack

---
+++ Stack/NextGreaterElement.java
@@ -0,0 +1,29 @@
+// Problem: Next Greater Element
+// Platform: LeetCode
+// Topic: Stack (Monotonic Stack)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        for (int num : nums2) {
+            while (!st.isEmpty() && st.peek() < num) {
+                map.put(st.pop(), num);
+            }
+            st.push(num);
+        }
+
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return res;
+    }
+}
