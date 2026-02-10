2244ad1030f7d0dd8db7240aced712a8d3a6c36f
Ishika_Upadhyay <158724315+Ishika1614@users.noreply.github.com>
2026-02-11 01:24:28 +0530

    Update MaximumProductSubarray

--- Array/MaximumProductSubarray.java
+++ Array/MaximumProductSubarray.java
@@ -1,17 +1,1 @@
- import java.util.*;
- class Solution {
-    public int maxProduct(int[] nums) {  
 
-     int maxProduct = nums[0]; 
-     int currentProduct = nums[0]; 
-     int prevProduct = nums[0];
-      
-      for(int i = 1; i<nums.length; i++) {  
-        int temp = currentProduct;
-        currentProduct = Math.max(nums[i],  Math.max(nums[i]*currentProduct , nums[i] *prevProduct));  
-        prevProduct = Math.min(nums[i], Math.min(nums[i]*temp,nums[i]*prevProduct));
-        maxProduct = Math.max(maxProduct, currentProduct);
-      }
-      return maxProduct;
-    }
-}
