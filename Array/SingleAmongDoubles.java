08d1c790e9c345aed0d8fdea87dce61c46af41ee
Ishika_Upadhyay <158724315+Ishika1614@users.noreply.github.com>
2026-02-16 23:49:57 +0530

    AddedSingleAmongDoubles

---
+++ Array/SingleAmongDoubles.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+import java.util.*;
+class Sol {
+    public static int search(int n, int arr[]) {
+      HashMap<Integer, Integer> map = new HashMap<>(); 
+      for(int num : arr){ 
+          map.put(num, map.getOrDefault(num,0) +1);
+      } 
+      for(int key :map.keySet()){ 
+          if(map.get(key) == 1){ 
+              return key; 
+          } 
+      } 
+      return -1;
+        
+    }
+}
